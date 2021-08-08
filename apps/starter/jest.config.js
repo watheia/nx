40025323aa1a832e1e72f1eed72c4959ccb15fe6
Aaron Miller <amiller@watheia.org>
2021-08-08 17:54:02 +0000

    Initial Commit

---
+++ apps/starter/jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  displayName: 'starter',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
+    '^.+\\.[tj]sx?$': 'babel-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/apps/starter',
+};
